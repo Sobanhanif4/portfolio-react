1287d9c1399db762743d6004c7c45ac2efada148
Soban <sobanatban4@gmail.com>
2025-05-14 16:19:35 +0500

    Fix file casing for ChatWidget.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Projects from "../components/Projects"
 import Experience from "../components/Experience"
 import Skills from "../components/Skills"
 import Story from "../components/Story"
-import ChatWidget from "../components/chatWidget"
+import ChatWidget from "../components/ChatWidget"
 
 
 
